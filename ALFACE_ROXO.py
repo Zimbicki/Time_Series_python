b6b4ab89ea192d8cbea00215a4b9f39f3c70de14
Kaique Zimbicki <162515392+Zimbicki@users.noreply.github.com>
2025-08-21 20:21:21 -0300

    add Cabeçalho Script em python

---
+++ ALFACE_ROXO.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Nome do Arquivo: ALFACE_ROXO.py
+Autor: Kaique Zimbicki
+Data: 2023-10-01
+Descrição: Programa que informa o preço do alface roxo.
+Licença: MIT
+"""
+
+# Importa os modulos se necessario 
+
+# import sys
+
+# Função principal do programa
+
+
+# Garante que a função principal seja chamada quando o script for executado
